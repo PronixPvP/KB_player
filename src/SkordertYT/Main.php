b5ae54dc169c8178dcaea35ac2e1944b73f276e5
SkordertYT <83978949+SkordertYT@users.noreply.github.com>
2021-09-04 18:01:35 +0300

    Update Main.php

--- src/SkordertYT/Main.php
+++ src/SkordertYT/Main.php
@@ -1,1 +1,25 @@
+<?php
 
+namespace SkordertYT;
+
+use pocketmine\event\Listener as PronixPvP;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+
+class Main extends PluginBase implements PronixPvP
+{
+
+public function onStartKb(EntityDamageEvent $e){
+if($e instanceof EntityDamageByEntityEvent){
+$damager = $e->getDamager();
+$entity = $event->getEntity();
+if($damager instanceof Player){
+$event->setKnockback(0.46);
+$entity->motionY += 1;
+$entity->motionX = -1;
+} 
+}
+}
+
+}
